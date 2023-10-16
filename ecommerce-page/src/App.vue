3a661ae76ee7380b64e0f2d849ecd836cdbd0864
Guilherme Schardosim Scherer <gschardosim@gmail.com>
2023-10-16 08:57:01 -0300

    fix login page

--- ecommerce-page/src/App.vue
+++ ecommerce-page/src/App.vue
@@ -19,5 +19,11 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+
 </style>
--- ecommerce-page/src/views/LoginView.vue
+++ ecommerce-page/src/views/LoginView.vue
@@ -1,16 +1,16 @@
 <template>
 
     <div class="login-wrapper">
-        <div class="login container-sm mx-auto">
-            <div class="form-floating mb-3 col col-4">
+        <div class="login container-sm col-6">
+            <div class="form-floating mb-3 col">
                 <input type="email" class="form-control" id="floatingInput" placeholder="User">
                 <label for="floatingInput">User</label>
             </div>
-            <div class="form-floating col-4">
+            <div class="form-floating col">
                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
                 <label for="floatingPassword">Password</label>
             </div>
-            <RouterLink to="/" class="btn btn-outline-secondary btn-lg mt-3 col-4">Sign in</RouterLink>
+            <RouterLink to="/" class="btn btn-outline-secondary btn-lg mt-3 col-12">Sign in</RouterLink>
         </div>
     </div>
 
@@ -27,14 +27,9 @@ export default {
 <style scoped>
 .login-wrapper {
     background-color: #040015;
-    height: 100%;
-}
-
-.login {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
-    height: 100%;
+    flex: 1;
 }
 </style>
